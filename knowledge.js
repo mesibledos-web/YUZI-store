cbc93d3196a324298e4c186d3616aab56d6b001d
mesibledos-web <mesibledos@gmail.com>
2026-01-23 11:49:18 +0700

    Delete knowledge.js

--- knowledge.js
+++
@@ -1,7 +0,0 @@
-const KNOWLEDGE = {
-  ai: "AI adalah kecerdasan buatan yang meniru cara berpikir manusia.",
-  javascript: "JavaScript adalah bahasa pemrograman web interaktif.",
-  github: "GitHub Pages adalah hosting gratis untuk website statis.",
-  html: "HTML adalah struktur dasar halaman web.",
-  css: "CSS digunakan untuk desain dan tampilan web."
-};
